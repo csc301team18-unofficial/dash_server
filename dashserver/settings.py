6cd00b1eadd74add2470497a129ab1de399b4ec9
Abhishek Dash <abhidash29@gmail.com>
2018-03-21 18:50:13 -0400

    Added models.py and (maybe) made a fix to settings.py

--- dashserver/settings.py
+++ dashserver/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
-
 # Connect to Heroku-PostgreSQL URL
 db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
