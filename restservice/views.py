9cb8fc923d51abf4bed2a3efd8626af4ca6a2c7c
Abhishek Dash <abhidash29@gmail.com>
2018-03-22 15:43:47 -0400

    started work on JSON rendering

--- restservice/views.py
+++ restservice/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+
+#from
+
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+#@csrf_exempt
+#def food_info(request, food_name):
+
