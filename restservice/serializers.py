44d761ced9fdf459354976f965380bcf02ae91bb
Abhishek Dash <abhidash29@gmail.com>
2018-03-23 20:15:10 -0400

    Created serializer for FoodCache.

---
+++ restservice/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from restservice.models import *
+
+
+class FoodCacheSerializer(serializers.Serializer):
+    food_hash = serializers.CharField(read_only=True, max_length=32)
+    food_name = serializers.CharField(max_length=100)
+    kilocalories = serializers.IntegerField()
+    fat_grams = serializers.IntegerField()
+    carb_grams = serializers.IntegerField()
+    protein_grams = serializers.IntegerField()
+
+    def create(self, validated_data):
+        food_cache = FoodCache(**validated_data)
+        food_cache.save()
+        return food_cache
+
+    def update(self, instance, validated_data):
+        instance.food_name = validated_data.get('name', instance.name)
+        instance.kilocalories = validated_data.get('kilocalories', instance.name)
+        instance.fat_grams = validated_data.get('fat_grams', instance.name)
+        instance.carb_grams = validated_data.get('carb_grams', instance.name)
+        instance.protein_grames = validated_data.get('protein_grams', instance.name)
+        instance.save()
+        return instance
