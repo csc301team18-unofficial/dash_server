5889271c0842966b2f37c2286d5e98511a50d460
Abhishek Dash <abhidash29@gmail.com>
2018-03-24 17:54:22 -0400

    Fixed Nutritics request verb

--- nutrition/nutriconstants.py
+++ nutrition/nutriconstants.py
@@ -1,7 +1,7 @@
 NUTRITICS_USER = "dashserver6"
 NUTRITICS_PSWD = "csc301!!"
 
-FOOD_BASE_URL = "https://www.nutritics.com/api/v1.1/&food="
+FOOD_BASE_URL = "https://www.nutritics.com/api/v1.1/LIST/&food="
 ALL_ATTRS = "&attr=name,energyKcal,carbohydrate,protein,fat"
 PROTEIN_ATTR = "&attr=name,protein"
 FAT_ATTR = "&attr=name,fat"
