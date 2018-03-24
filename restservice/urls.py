a3846b2b164f138cfb68a15a9dc7f40ecc46055f
Abhishek Dash <abhidash29@gmail.com>
2018-03-24 17:26:47 -0400

    Added a slash to the regex

--- restservice/urls.py
+++ restservice/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url
 from restservice import views
 
 urlpatterns = [
-    url(r'^foodinfo/client_id=(?P<client_id>.+?)&food_name=(?P<food_name>.+?)$', views.food_info)
+    url(r'^foodinfo/client_id=(?P<client_id>.+?)&food_name=(?P<food_name>.+?)/$', views.food_info)
 ]
