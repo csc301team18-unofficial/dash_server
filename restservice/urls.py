9c4d81e8ebe9d6dd4ecbeee5f472c29d2b0c5974
gabmorg <gab.morgenshtern@gmail.com>
2018-03-23 21:53:15 -0400

    added url import

--- restservice/urls.py
+++ restservice/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     url(r'^rest/foodinfo/', views.food_info)
