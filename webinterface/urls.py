ac0ef5cb137fcdb0f9f69e469c147b2da9f261f2
Abhishek Dash <abhidash29@gmail.com>
2018-03-28 00:03:05 -0400

    Added webinterface code

--- webinterface/urls.py
+++ webinterface/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from webinterface import views
 
 urlpatterns = [
+<<<<<<< HEAD
+    url(r'^(?P<client_name>.+?)/$', views.insights, name='insights')
+	]
+=======
     url(r'^(?P<client_name>.+?)/$', views.serve_client)
 ]
+>>>>>>> c247437c9a9fb797f15bad5a00e43fa428c11f32
--- webinterface/views.py
+++ webinterface/views.py
@@ -9,12 +9,20 @@ from restservice.serializers import *
 from utility.utils import *
 
 
+<<<<<<< HEAD
+def insights(request, client_name):
+=======
 def serve_client(request, client_name):
+>>>>>>> c247437c9a9fb797f15bad5a00e43fa428c11f32
     try:
         user_entry = Users.objects.get(name=client_name)
     except ObjectDoesNotExist:
         # TODO: this happens if the user doesn't have an account, give them an error page or something
         pass
 
+<<<<<<< HEAD
+    return render(request, 'webinterface/templates/insights/insights.html')
+=======
     # TODO: Render the page for this user
     pass
+>>>>>>> c247437c9a9fb797f15bad5a00e43fa428c11f32
